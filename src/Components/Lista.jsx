54c7b8867c4aeb8be1c06d8a1225b0dd574deecc
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-09-13 21:39:40 -0500

    cambios

--- src/Components/Lista.jsx
+++ src/Components/Lista.jsx
@@ -4,33 +4,33 @@ import { useEffect } from 'react'
 export default function Lista() {
     const [pokemones, setPokemones] = useState([])
 
-    useEffect(() => {   
-           fetch('https://pokeapi.co/api/v2/pokemon/?limit=25')
-            .then(r => r.json() )
-            .then( pokemon => {  
+    let url = "https://pokeapi.co/api/v2/pokemon/?limit=25"
+
+    useEffect(() => {          
+        fetch( url)  
+        .then(r => r.json() )
+        .then( pokemon => {  
                                     
                 setPokemones(pokemon.results)
+                
             })
                
     },[])
     return (
         <section>
-            <table>
+            <table className="tabla" border="2">
                 <thead>
                     <tr>
                         <th>Nombre</th>
-                        <th>Imagen</th>
-                        <th>Tipo o Tipos</th>
-                        <th>Id</th>
+                        <th>Imagen</th>                        
                     </tr>
                 </thead>
                 <tbody>
                     {pokemones.map ((poke, index) => {
                         return <tr key={index}>
                             <td>{poke.name}</td>
-                            <td><img  src={`https://pokeres.bastionbot.org/images/pokemon/${index +1}.png`} alt="Nombre Pokemon" /></td>
-                                                       
-                        </tr>
+                            <td><img className="imagenes_25" src={`https://pokeres.bastionbot.org/images/pokemon/${index +1}.png`} alt="Nombre Pokemon" /></td>
+                            </tr>
                     })}
                 </tbody>
             </table>
